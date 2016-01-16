c03ea22707377e17463a84a6f57f0c19cbf0d267
mosschief <mosschief@gmail.com>
2016-01-15 16:20:38 -0800

    add look up

--- motor.py
+++ motor.py
@@ -1,4 +1,5 @@
-from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
+from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor, Adafruit_StepperM$
+
 import time
 
 mh = Adafruit_MotorHAT(addr=0x60)
@@ -6,12 +7,14 @@ mh = Adafruit_MotorHAT(addr=0x60)
 # Motor 1 = Left, Motor 2 = Right
 motor1 = mh.getMotor(1)
 motor2 = mh.getMotor(2)
+motor3 = mh.getStepper(100,2)
 
 # set the speed to start, from 0 (off) to 255 (max speed)
 
 
 motor1.setSpeed(150)
 motor2.setSpeed(150)
+motor3.setSpeed(30)
 
 def leftForward():
     motor1.run(Adafruit_MotorHAT.FORWARD)
@@ -55,3 +58,9 @@ def speedDown(speed):
         return speed
     else:
         return "Minimum Speed"
+
+def lookUp():
+    motor3.step(2, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.SINGLE)
+
+def lookDown():
+    motor3.step(2, Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.SINGLE)
