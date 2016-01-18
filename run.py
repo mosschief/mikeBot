7c5e60e7118de307411badc0c103bc8f71b1cfdd
mosschief <mosschief@gmail.com>
2016-01-18 15:04:06 -0800

    add cross domain access

--- run.py
+++ run.py
@@ -20,7 +20,7 @@ basic_auth = BasicAuth(app)
 
 def startCamera():
     os.system("mkdir /tmp/stream")
-    os.system("raspistill --nopreview -w 640 -h 480 -q 5 -fps 15 -o /tmp/stream/pic.jpg -tl 100 -t 9999999 -th 0:0:0 &")
+    os.system("raspistill --nopreview -w 640 -h 480 -q 5 -f 15 -o /tmp/stream/pic.jpg -tl 100 -t 9999999 -th 0:0:0 &")
 
 
 def startStream():
