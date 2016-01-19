3a13a25bea5f350933c981baeb2c6bf39b6b2860
mosschief <mosschief@gmail.com>
2016-01-18 19:03:47 -0800

    change to experiemental

--- run.py
+++ run.py
@@ -20,8 +20,8 @@ basic_auth = BasicAuth(app)
 
 def startStream():
     os.chdir('/var/www/mjpg-streamer/mjpg-streamer-experimental')
-    os.system('export LD_LIBRARY_PATH=/var/www/mjpg-streamer/mjpg-streamer-experimental')
-    os.system('./mjpg_streamer -o "output_http.so -w ./www" -i "input_raspicam.so" -vf -hf')
+    os.system('export LD_LIBRARY_PATH=.')
+    os.system('./mjpg_streamer -o "output_http.so -w ./www" -i "input_raspicam.so -fps 15 -vf -hf"')
 
 
 @app.route('/', methods=['GET','POST'])
